a9e18493f83c2cdb049c5c945a48409675f4f78d
HyeokJu Lee <dlhjwwe@naver.com>
2024-02-25 00:20:39 +0900

    modify FaqServiceTest

--- src/test/java/com/bookitaka/NodeulProject/faq/FaqServiceTest.java
+++ src/test/java/com/bookitaka/NodeulProject/faq/FaqServiceTest.java
@@ -17,34 +17,10 @@ class FaqServiceTest {
     final String answer = "answer";
     final String category = "category";
     final String update = "update";
+    final int insertNum = 2;
 
     @Test
-    public void register_remove() {
-        // register
-        Faq faq1 = new Faq();
-        faq1.setFaqQuestion(question);
-        faq1.setFaqAnswer(answer);
-        faq1.setFaqCategory(category);
-        faq1.setFaqBest(1);
-        Faq faq2 = new Faq(null, question, answer, category, 0, null, null);
-        faqService.registerFaq(faq1);
-        faqService.registerFaq(faq2);
-
-        // findOneFaq 단건 조회
-        Faq findFaq1 = faqService.getOneFaq(faq1.getFaqNo()).get();
-        Faq findFaq2 = faqService.getOneFaq(faq2.getFaqNo()).get();
-        assertThat(findFaq1.getFaqQuestion()).isEqualTo(faq1.getFaqQuestion());
-        assertThat(findFaq2.getFaqQuestion()).isEqualTo(faq2.getFaqQuestion());
-
-        // removeFaq 삭제 검증
-        faqService.removeFaq(faq1);
-        faqService.removeFaq(faq2);
-        long deletedcount = faqService.countFaq();
-        assertThat(deletedcount).isEqualTo(0);
-    }
-
-    @Test
-    public void modify() {
+    public void registerFaq_getOneFaq_modifyFaq_countFaq_removeFaq() {
         // register
         Faq faq1 = new Faq();
         faq1.setFaqQuestion(question);
@@ -74,14 +50,27 @@ class FaqServiceTest {
         faqService.modifyFaq(newFaq1);
         faqService.modifyFaq(newFaq2);
 
-        System.out.println("Service - modify 호출 후: 번호" + findFaq1.getFaqNo() + "질문" + findFaq1.getFaqQuestion());
-
         Faq updateFaq1 = faqService.getOneFaq(findFaq1.getFaqNo()).get();
         Faq updateFaq2 = faqService.getOneFaq(findFaq2.getFaqNo()).get();
-        System.out.println("Service - modify 후 DB에서 getOne: 번호" + updateFaq1.getFaqNo() + "질문" + updateFaq1.getFaqQuestion());
+
+        System.out.println("faq1.getFaqNo() : " + faq1.getFaqNo());
+        System.out.println("faq1.getFaqQuestion() : " + faq1.getFaqQuestion());
+        System.out.println("findFaq1.getFaqNo() : " + findFaq1.getFaqNo());
+        System.out.println("findFaq1.getFaqQuestion() : " + findFaq1.getFaqQuestion());
+        System.out.println("newFaq1.getFaqNo() : " + newFaq1.getFaqNo());
+        System.out.println("newFaq1.getFaqQuestion() : " + newFaq1.getFaqQuestion());
+        System.out.println("updateFaq1.getFaqNo() : " + updateFaq1.getFaqNo());
+        System.out.println("updateFaq1.getFaqQuestion() : " + updateFaq1.getFaqQuestion());
 
         assertThat(updateFaq1.getFaqQuestion()).isEqualTo(newFaq1.getFaqQuestion());
         assertThat(updateFaq2.getFaqQuestion()).isEqualTo(newFaq2.getFaqQuestion());
+
+        // removeFaq 삭제 검증
+        long beforeCount = faqService.countFaq();
+        faqService.removeFaq(faq1);
+        faqService.removeFaq(faq2);
+        long deletedcount = faqService.countFaq();
+        assertThat(deletedcount).isEqualTo(beforeCount - insertNum);
     }
 
 //    @Test
