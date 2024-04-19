e1ceecc543c7b1d53cb19cdc5581fd9cc1237914
HyeokJu Lee <dlhjwwe@naver.com>
2024-04-19 17:53:33 +0900

    application.yml 수정

--- src/main/java/com/bookitaka/NodeulProject/Application.java
+++ src/main/java/com/bookitaka/NodeulProject/Application.java
@@ -1,24 +1,22 @@
 package com.bookitaka.NodeulProject;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.context.annotation.Bean;
 
 //@ImportAutoConfiguration
 @EnableJpaAuditing
 @SpringBootApplication
-public class NodeulProjectApplication {
+public class Application {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
 			+ "/app/config/springboot-webservice/real-application.yml";
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(NodeulProjectApplication.class)
+		new SpringApplicationBuilder(Application.class)
 				.properties(APPLICATION_LOCATIONS)
 				.run(args);
 	}
